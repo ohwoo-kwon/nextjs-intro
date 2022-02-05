050ab8c5c9a46deaa2a10c7540c3491fb82840f4
ohwoo-kwon <kwonow1@gmail.com>
2022-02-05 19:24:10 +0900

    1.6 Custom App

--- components/NavBar.js
+++ components/NavBar.js
@@ -13,15 +13,12 @@ export default function NavBar() {
         <a className={router.pathname === "/about" ? "active" : ""}>About</a>
       </Link>
       <style jsx>{`
-        nav {
-          background-color: tomato;
-        }
         a {
           text-decoration: none;
         }
 
         .active {
-          color: yellow;
+          color: tomato;
         }
       `}</style>
     </nav>
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,9 @@
-import { useState } from "react";
 import NavBar from "../components/NavBar";
 
 export default function Home() {
-  const [counter, setCounter] = useState(0);
   return (
     <div>
-      <NavBar />
-      <h1>Hello {counter}</h1>
-      <button onClick={() => setCounter((prev) => prev + 1)}>+</button>
+      <h1>Hello</h1>
     </div>
   );
 }
